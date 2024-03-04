39d330be9331a798d6582a7c15dc29f74e09d676
datnguyentien0401 <dat.nt0401@gmail.com>
2024-03-04 10:30:19 +0700

    Update ProjecMntServerApplicationTests.java

--- src/test/java/com/example/projecmntserver/ProjecMntServerApplicationTests.java
+++ src/test/java/com/example/projecmntserver/ProjecMntServerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.projecmntserver;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ProjecMntServerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.projecmntserver;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ProjecMntServerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
